ba15c2976bbd106d59fc15577066e0332ef06c52
huy-raw <ruacon130@gmail.com>
2021-10-05 12:44:23 +0700

    fix id injection

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -10,8 +10,8 @@ package utils;
  * @author HuyRaw
  */
 public class Constants {
-    public static final String REGEX_INJECTION_ID = "^[a-zA-Z]{2}\\d{6}$";
-    public static final String REGEX_STUDENT_ID = "^[s|S]{1}[e|E]{1}\\d{6}$";
+    public static final String REGEX_INJECTION_ID = "^[I]{1}\\d{6}$";
+    public static final String REGEX_STUDENT_ID = "^[s|S]{1}[e|E|a|A|s|S]{1}\\d{6}$";
     public static final String REGEX_VACCINE_ID = "^[c|C]{1}[o|O]{1}[v|V]{1}[i|I]{1}[d|D]{1}[-]{1}[v|V]{1}\\d{3}$";
     public static final String DATE_FORMAT = "dd/MM/yyyy";
     public static final String DATE_REGEX = "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9]|[0-9])/[0-9]{4}$";
